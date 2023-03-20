7843233591c12bb4dcf5c2f541b4521c509e65f9
akruglov18 <a.kruglov.2001@mail.ru>
2023-03-20 23:43:49 +0300

    new vector

---
+++ containers/HashMap.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+#include <list>
+
+template<typename KeyType, typename ValueType>
+class HashMap
+{
+private:
+    std::vector<std::list<std::pair<KeyType, ValueType>>> data;
+
+public:
+
+    void insert(Value value)
+    {
+
+    }
+
+    void erase(Value value)
+    {
+
+    }
+
+};
---
+++ containers/ParallelVector4.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <mutex>
+#include <shared_mutex>
+#include <atomic>
+
+template<typename T>
+class ParallelVector4
+{
+private:
+    T* data;
+    std::atomic_size_t size;
+    size_t capacity;
+    std::shared_mutex mut;
+public:
+    ParallelVector4() : data(nullptr), size(0), capacity(0)
+    {
+    }
+
+    void push_back(const T& value)
+    {
+        size_t num_elem = size.fetch_add(1);
+
+        if (num_elem >= capacity)
+        {
+            std::unique_lock<std::shared_mutex> lock(mut);
+            if (num_elem >= capacity) {
+                size_t new_capacity = capacity * 2 + 1;
+                T* new_data = new int[new_capacity];
+                for (int i = 0; i < capacity; i++)
+                {
+                    new_data[i] = data[i];
+                }
+                delete[] data;
+                data = new_data;
+                capacity = new_capacity;
+            }
+        }
+        std::shared_lock<std::shared_mutex> lock(mut);
+        data[num_elem] = value;
+    }
+
+    size_t get_size() const
+    {
+        return size;
+    }
+
+    T& operator[](size_t i)
+    {
+        return data[i];
+    }
+
+    const T& operator[](size_t i) const
+    {
+        return data[i];
+    }
+    
+    void write(size_t index, const T& value)
+    {
+        std::shared_lock<std::shared_mutex> lock(mut);
+        data[index] = value;
+    }
+
+    T read(size_t index)
+    {
+        std::shared_lock<std::shared_mutex> lock(mut);
+        return data[index];
+    }
+
+    ~ParallelVector4()
+    {
+        delete[] data;
+    }
+};

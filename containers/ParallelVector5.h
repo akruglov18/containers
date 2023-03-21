a1ce4da2be52c0c73008dcda8ebcdf754c7cbff8
akruglov18 <a.kruglov.2001@mail.ru>
2023-03-21 22:00:59 +0300

    added another vector

---
+++ containers/ParallelVector5.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <mutex>
+#include <list>
+#include <atomic>
+#include <shared_mutex>
+
+template<typename T, size_t block_size>
+class ParallelVector5
+{
+private:
+    T** data;
+    std::atomic_size_t size;
+    size_t capacity;
+    size_t ptr_count;
+    std::shared_mutex mut;
+
+public:
+    ParallelVector5() : data(nullptr), size(0), capacity(0), ptr_count(0)
+    {
+    }
+
+    void push_back(const T& val)
+    {
+        size_t num_elem = size.fetch_add(1);
+        while (size >= capacity) {
+            std::unique_lock<std::shared_mutex> lock(mut);
+            while (size >= capacity) {
+                addNewBlock();
+            }
+        }
+        std::shared_lock<std::shared_mutex> lock(mut);
+        data[num_elem / block_size][num_elem % block_size] = val;
+    }
+
+    T& operator[](std::size_t i)
+    {
+        std::shared_lock<std::shared_mutex> lock(mut);
+        return data[i / block_size][i % block_size];
+    }
+
+    const T& operator[](std::size_t i) const
+    {
+        std::shared_lock<std::shared_mutex> lock(mut);
+        return data[i / block_size][i % block_size];
+    }
+
+    void clear()
+    {
+        for (size_t i = 0; i < ptr_count; i++)
+        {
+            delete[] data[i];
+        }
+        delete[] data;
+        size = 0;
+        capacity = 0;
+        ptr_count = 0;
+    }
+
+    size_t get_size() const
+    {
+        return size;
+    }
+
+    ~ParallelVector5()
+    {
+        clear();
+    }
+
+private:
+    void addNewBlock()
+    {
+        T** newData = new T * [ptr_count + 1];
+        for (size_t i = 0; i < ptr_count; i++)
+        {
+            newData[i] = data[i];
+        }
+        delete[] data;
+        data = newData;
+        T* newBLock = new T[block_size];
+        data[ptr_count] = newBLock;
+        ptr_count++;
+        capacity += block_size;
+    }
+};
--- containers/src.cpp
+++ containers/src.cpp
@@ -1,3 +1,5 @@
 #include "ParallelVector.h"
 #include "ParallelVector2.h"
 #include "ParallelVector3.h"
+#include "ParallelVector4.h"
+#include "ParallelVector5.h"
